7c796de9891179985077c76bc0ec534a8be0e078
ShubhamB2595 <shubhambhele95@gmail.com>
2022-01-02 13:06:18 +0530

    Code ADDED for methods of webdriver interface

--- src/main/java/com/sample/webdriver/WebDriverMethods.java
+++ src/main/java/com/sample/webdriver/WebDriverMethods.java
@@ -9,12 +9,36 @@ public class WebDriverMethods {
 
         // System Property for Chrome Driver
         System.setProperty("webdriver.chrome.driver" , "I:\\Installation Files\\chromedriver.exe");
+
+        // calling the webdriver interface method
+        interfaceMethods();
+    }
+
+    // Methods of webdriver interface
+    static void interfaceMethods() {
+
         // Instantiate a ChromeDriver class
         WebDriver driver = new ChromeDriver();
         // Launching website
         driver.get("http://google.com/");
         // Maximize the browser window
         driver.manage().window().maximize();
+        // get the title of page
+        System.out.println("The title of page: " + driver.getTitle());
+        // get the pagesource
+        System.out.println("The pagesource: " + driver.getPageSource());
+        // get the current page url
+        System.out.println("The current page url: " + driver.getCurrentUrl());
+        // get the window handle
+        System.out.println("The window handle: " + driver.getWindowHandle());
+        // get the window handles
+        System.out.println("The window handles: " + driver.getWindowHandles());
+        // Navigating to new website
+        driver.navigate().to("https://www.facebook.com/");
+        // Navigating to previous website
+        driver.navigate().back();
+        // Navigating to next website
+        driver.navigate().forward();
         // Close the chrome window
         driver.close();
     }
